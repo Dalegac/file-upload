4b8095cbfb81c302016b9529e70a349e41f058f1
Dale <1175204449@qq.com>
2021-04-28 18:40:35 +0800

    fix: 调试运行

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -1,89 +1,86 @@
 // app/controller/home.js
-const path = require('path')
-const fse = require("fs-extra")
+const path = require("path");
+const fse = require("fs-extra");
 
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class HomeController extends Controller {
   async index() {
     this.ctx.body = {
-      msg:'hello eggjs'
-    }
+      msg: "hello eggjs",
+    };
   }
-  async merge(){
-    const {ext,size,hash} = this.ctx.request.body
-    const filePath = path.resolve(this.config.UPLOAD_DIR, `${hash}.${ext}`)
-    await this.ctx.service.upload.mergeFileChunk(filePath, hash, size)
+  async merge() {
+    const { ext, size, hash } = this.ctx.request.body;
+    const filePath = path.resolve(this.config.UPLOAD_DIR, `${hash}.${ext}`);
+    await this.ctx.service.upload.mergeFileChunk(filePath, hash, size);
     this.ctx.body = {
-      code:0,
-      msg:'合并成功'
-    }
-
+      code: 0,
+      msg: "合并成功",
+    };
   }
-  async getUploadedList(dirPath){
-    return fse.existsSync(dirPath) 
-      ? (await fse.readdir(dirPath)).filter(name=>name[0]!=='.') // 过滤诡异的隐藏文件 比如.DS_store
-      : []
+  async getUploadedList(dirPath) {
+    return fse.existsSync(dirPath)
+      ? (await fse.readdir(dirPath)).filter((name) => name[0] !== ".") // 过滤诡异的隐藏文件 比如.DS_store
+      : [];
   }
-  async check(){
-    const { ext, hash } = this.ctx.request.body
-    const filePath = path.resolve(this.config.UPLOAD_DIR, `${hash}.${ext}`)
-    console.log(filePath)
+  async check() {
+    const { ext, hash } = this.ctx.request.body;
+    const filePath = path.resolve(this.config.UPLOAD_DIR, `${hash}.${ext}`);
+    console.log(filePath);
     // 文件是否存在
-    let uploaded = false
-    let uploadedList = []
+    let uploaded = false;
+    let uploadedList = [];
     if (fse.existsSync(filePath)) {
       // 存在文件，直接返回已上传
-      uploaded = true
-    }else{
+      uploaded = true;
+    } else {
       // 文件没有完全上传完毕，但是可能存在部分切片上传完毕了
-      uploadedList = await this.getUploadedList(path.resolve(this.config.UPLOAD_DIR, hash))
+      uploadedList = await this.getUploadedList(
+        path.resolve(this.config.UPLOAD_DIR, hash)
+      );
     }
 
     this.ctx.body = {
-      code:0,
+      code: 0,
       uploaded,
-      uploadedList // 过滤诡异的隐藏文件
-    }
-  } 
-  async upload(){
-    const { ctx } = this
-    if(Math.random()<0.5){
-      // 随机报个错
-      return ctx.status = 500;
-    }
-    const file = ctx.request.files[0]
-    const {chunkname,ext,hash} = ctx.request.body
-    console.log(file,hash,chunkname,ext)
-    const filename = `${hash}.${ext}`
+      uploadedList, // 过滤诡异的隐藏文件
+    };
+  }
+  async upload() {
+    const { ctx } = this;
+    // if(Math.random()<0.5){
+    //   // 随机报个错
+    //   return ctx.status = 500;
+    // }
+    console.log(ctx);
+    const file = ctx.request.files[0];
+    const { chunkname, ext, hash } = ctx.request.body;
+    console.log(file, hash, chunkname, ext);
+    const filename = `${hash}.${ext}`;
     // 最终文件存储位置 根据chunkname获取后缀，名字用hash
-    const filePath = path.resolve(
-      this.config.UPLOAD_DIR,
-      filename
-    )
+    const filePath = path.resolve(this.config.UPLOAD_DIR, filename);
     // 碎片文件夹，用hash命名
-    const chunkPath = path.resolve(this.config.UPLOAD_DIR, hash )
+    const chunkPath = path.resolve(this.config.UPLOAD_DIR, hash);
 
     // 文件存在直接返回
     if (fse.existsSync(filePath)) {
       this.ctx.body = {
-        code:-1,
-        msg:'文件存在',
-        url:`/public/${filename}`
-      }
-      return
+        code: -1,
+        msg: "文件存在",
+        url: `/public/${filename}`,
+      };
+      return;
     }
     if (!fse.existsSync(this.config.UPLOAD_DIR)) {
-      await fse.mkdirs(this.config.UPLOAD_DIR)
+      await fse.mkdirs(this.config.UPLOAD_DIR);
     }
-    await fse.move(file.filepath, `${chunkPath}/${chunkname}`)
-    this.ctx.body={
-      code:0,
-      msg:'上传成功',
-      url:`/public/${filename}`
-    }
-
-
+    await fse.move(file.filepath, `${chunkPath}/${chunkname}`);
+    this.ctx.body = {
+      code: 0,
+      msg: "上传成功",
+      url: `/public/${filename}`,
+    };
   }
 }
 
--- server/config/config.default.js
+++ server/config/config.default.js
@@ -1,23 +1,55 @@
 // config/config.default.js
-const path = require('path')
-module.exports = appInfo => {
+const path = require("path");
+module.exports = (appInfo) => {
   const config = {};
-  config.keys = 'shengxinjign@!#rocks!';
+  config.keys = "shengxinjign@!#rocks!";
   // config.middleware = ['cors'];
-  config.multipart = {
-    mode: 'file',
-    // multipart: {
-    // },
-      whitelist: ()=>true
-
-
-  }
   config.security = {
     csrf: {
-      enable: false
+      enable: false,
     },
-  }
+  };
   config.UPLOAD_DIR = path.resolve(__dirname, "..", "app/public"); // 大文件存储目录
 
+  config.multipart = {
+    mode: "file",
+    // fieldNameSize: 100, //字段名称长度限制     fieldSize: '1mb', //字段值长度大小限制，比如富文本有时添加多媒体后就会很大，此处要调高限制。
+    // fields: 50, //字段数量限制
+    fileSize: "200mb", //上传文件大小限制
+    whitelist: [
+      //无拓展名文件上传
+      "",
+      // images 支持
+      ".jpg",
+      ".jpeg",
+      ".png",
+      ".gif",
+      ".bmp",
+      ".wbmp",
+      ".webp",
+      ".tif",
+      ".psd", // text
+
+      ".svg",
+      ".js",
+      ".jsx",
+      ".json",
+      ".css",
+      ".less",
+      ".html",
+      ".htm",
+      ".xml", // tar
+
+      ".zip",
+      ".gz",
+      ".tgz",
+      ".gzip", // video
+
+      ".mp3",
+      ".mp4",
+      ".avi",
+    ],
+    // files: 10, //上传文件数量限制
+  };
   return config;
 };
